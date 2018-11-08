bcf94ea70ee780e1a14a70b389452ee2cfc80c31
Kateryna <nakonechna.katja@gmail.com>
2018-11-08 10:30:57 +0200

    add registration

--- controllers/mobile/frmLoginController/frmLoginController.js
+++ controllers/mobile/frmLoginController/frmLoginController.js
@@ -1,7 +1,8 @@
 define({ 
   
-
-  
-
+registration: function(){
+   this.view.btnGroup.isVisible = false;
+   this.view.flxReistration.isVisible = true;
+}
  });
   
--- controllers/mobile/frmLoginController/frmLoginControllerActions.js
+++ controllers/mobile/frmLoginController/frmLoginControllerActions.js
@@ -2,4 +2,13 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onOrientationChange defined for frmLogin **/
+    AS_Form_bce8f418ade1455d8be2bdddf93637b1: function AS_Form_bce8f418ade1455d8be2bdddf93637b1(eventobject) {
+        var self = this;
+    },
+    /** onClick defined for HyperLinkButton **/
+    AS_FlexContainer_ddcf7376c8904323823d9b086794be0e: function AS_FlexContainer_ddcf7376c8904323823d9b086794be0e(eventobject) {
+        var self = this;
+        return self.registration.call(this);
+    }
 });
