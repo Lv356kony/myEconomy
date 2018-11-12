9b7668e4b28b0c2a1ddf60870701cb729fa22558
Kateryna <nakonechna.katja@gmail.com>
2018-11-12 17:37:32 +0200

    add Login and Registration form

--- controllers/mobile/frmLoginController/frmLoginControllerActions.js
+++ controllers/mobile/frmLoginController/frmLoginControllerActions.js
@@ -2,13 +2,27 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onClick defined for btnLogin **/
+    AS_Button_f6d56250f0b74682be45ea8066f6ae20: function AS_Button_f6d56250f0b74682be45ea8066f6ae20(eventobject) {
+        var self = this;
+        return self.login.call(this);
+    },
+    /** onClick defined for flxButtonLogin **/
+    AS_FlexContainer_h15bd3f6ad58418b8b907a20ba67f773: function AS_FlexContainer_h15bd3f6ad58418b8b907a20ba67f773(eventobject) {
+        var self = this;
+    },
+    /** onClick defined for btnCreateAccount **/
+    AS_Button_a81613dad79044328c92a13c25dbd9d6: function AS_Button_a81613dad79044328c92a13c25dbd9d6(eventobject) {
+        var self = this;
+        return self.createAccount.call(this);
+    },
+    /** onClick defined for btnRegister **/
+    AS_Button_c79901a8efea4817b63ff4e68ce87ba9: function AS_Button_c79901a8efea4817b63ff4e68ce87ba9(eventobject) {
+        var self = this;
+        return self.registration.call(this);
+    },
     /** onOrientationChange defined for frmLogin **/
     AS_Form_bce8f418ade1455d8be2bdddf93637b1: function AS_Form_bce8f418ade1455d8be2bdddf93637b1(eventobject) {
         var self = this;
-    },
-    /** onClick defined for HyperLinkButton **/
-    AS_FlexContainer_ddcf7376c8904323823d9b086794be0e: function AS_FlexContainer_ddcf7376c8904323823d9b086794be0e(eventobject) {
-        var self = this;
-        return self.registration.call(this);
     }
 });
--- userwidgets/EF.PwdBxIcn/modules/require/PwdBxIcnControllerActions.js
+++ userwidgets/EF.PwdBxIcn/modules/require/PwdBxIcnControllerActions.js
@@ -2,27 +2,29 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onBeginEditing defined for txtPassword **/
     AS_TextField_79de7b9dbc164788a561b36cb34b3d2f: function AS_TextField_79de7b9dbc164788a561b36cb34b3d2f(eventobject, changedtext) {
         var self = this;
         if (kony.theme.getCurrentTheme() != "default") {
             kony.theme.setCurrentTheme("default", function() {
-                self.view.frmPwdName["skin"] = "skPWDSFocus";
+                self.view.frmPwdName.skin = "skPWDSFocus";
             }, null);
         } else {
             (function() {
-                self.view.frmPwdName["skin"] = "skPWDSFocus";
+                self.view.frmPwdName.skin = "skPWDSFocus";
             })();
         }
     },
+    /** onEndEditing defined for txtPassword **/
     AS_TextField_06ab01183a50442b9c2f262c92c0b649: function AS_TextField_06ab01183a50442b9c2f262c92c0b649(eventobject, changedtext) {
         var self = this;
         if (kony.theme.getCurrentTheme() != "default") {
             kony.theme.setCurrentTheme("default", function() {
-                self.view.frmPwdName["skin"] = "skPWDSNormal";
+                self.view.frmPwdName.skin = "skPWDSNormal";
             }, null);
         } else {
             (function() {
-                self.view.frmPwdName["skin"] = "skPWDSNormal";
+                self.view.frmPwdName.skin = "skPWDSNormal";
             })();
         }
         //Password validation Logic
@@ -47,27 +49,29 @@ define({
             }
         }
     },
+    /** onBeginEditing defined for txtPassword **/
     AS_TextField_a5b8da3c57124ccabc79daff58d85b3d: function AS_TextField_a5b8da3c57124ccabc79daff58d85b3d(eventobject, changedtext) {
         var self = this;
         if (kony.theme.getCurrentTheme() != "default") {
             kony.theme.setCurrentTheme("default", function() {
-                self.view.frmPwdName["skin"] = "skPWDSFocus";
+                self.view.frmPwdName.skin = "skPWDSFocus";
             }, null);
         } else {
             (function() {
-                self.view.frmPwdName["skin"] = "skPWDSFocus";
+                self.view.frmPwdName.skin = "skPWDSFocus";
             })();
         }
     },
+    /** onEndEditing defined for txtPassword **/
     AS_TextField_d30c59624cb94b9ca91bf71ab96536f8: function AS_TextField_d30c59624cb94b9ca91bf71ab96536f8(eventobject, changedtext) {
         var self = this;
         if (kony.theme.getCurrentTheme() != "default") {
             kony.theme.setCurrentTheme("default", function() {
-                self.view.frmPwdName["skin"] = "skPWDSNormal";
+                self.view.frmPwdName.skin = "skPWDSNormal";
             }, null);
         } else {
             (function() {
-                self.view.frmPwdName["skin"] = "skPWDSNormal";
+                self.view.frmPwdName.skin = "skPWDSNormal";
             })();
         }
         //Password validation Logic
