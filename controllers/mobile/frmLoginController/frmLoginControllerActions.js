ac9592c939f416ae7fe19f038d5c698bf4f2c964
Kateryna <nakonechna.katja@gmail.com>
2018-11-13 12:02:39 +0200

    add createAccount button

--- controllers/mobile/frmLoginController/frmLoginControllerActions.js
+++ controllers/mobile/frmLoginController/frmLoginControllerActions.js
@@ -11,11 +11,6 @@ define({
     AS_FlexContainer_h15bd3f6ad58418b8b907a20ba67f773: function AS_FlexContainer_h15bd3f6ad58418b8b907a20ba67f773(eventobject) {
         var self = this;
     },
-    /** onClick defined for btnCreateAccount **/
-    AS_Button_a81613dad79044328c92a13c25dbd9d6: function AS_Button_a81613dad79044328c92a13c25dbd9d6(eventobject) {
-        var self = this;
-        return self.createAccount.call(this);
-    },
     /** onClick defined for btnRegister **/
     AS_Button_c79901a8efea4817b63ff4e68ce87ba9: function AS_Button_c79901a8efea4817b63ff4e68ce87ba9(eventobject) {
         var self = this;
@@ -24,5 +19,10 @@ define({
     /** onOrientationChange defined for frmLogin **/
     AS_Form_bce8f418ade1455d8be2bdddf93637b1: function AS_Form_bce8f418ade1455d8be2bdddf93637b1(eventobject) {
         var self = this;
+    },
+    /** onClick defined for btnCreateAccount **/
+    AS_Button_bc68cf59db484e9b8031ca8335c09c68: function AS_Button_bc68cf59db484e9b8031ca8335c09c68(eventobject) {
+        var self = this;
+        return self.createAccount.call(this);
     }
 });
