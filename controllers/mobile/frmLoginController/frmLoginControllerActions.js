f329881118f6345adb23ed2fe2aa467eef3347d0
Kateryna <nakonechna.katja@gmail.com>
2018-11-16 19:23:30 +0200

    fix bugs

--- controllers/mobile/frmLoginController/frmLoginControllerActions.js
+++ controllers/mobile/frmLoginController/frmLoginControllerActions.js
@@ -2,6 +2,16 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onTextChange defined for txtEmail **/
+    AS_TextField_i7e412883bbb4daf967005bf2fd4b72f: function AS_TextField_i7e412883bbb4daf967005bf2fd4b72f(eventobject, changedtext) {
+        var self = this;
+        return self.cleanEmailError.call(this);
+    },
+    /** onTextChange defined for txtPassword **/
+    AS_TextField_d984052da1784ce5a9253faa6566876e: function AS_TextField_d984052da1784ce5a9253faa6566876e(eventobject, changedtext) {
+        var self = this;
+        return self.cleanPasswordError.call(this);
+    },
     /** onClick defined for btnCreateAccount **/
     AS_Button_bc68cf59db484e9b8031ca8335c09c68: function AS_Button_bc68cf59db484e9b8031ca8335c09c68(eventobject) {
         var self = this;
@@ -13,32 +23,22 @@ define({
         return self.login.call(this);
     },
     /** onClick defined for btnRegister **/
-    AS_Button_j57fa6913ee7438c90bc21fcebdcfcfd: function AS_Button_j57fa6913ee7438c90bc21fcebdcfcfd(eventobject) {
+    AS_Button_e881d3fb7e134719af2b1093133693b2: function AS_Button_e881d3fb7e134719af2b1093133693b2(eventobject) {
         var self = this;
         return self.registration.call(this);
+    },
+    /** onClick defined for btnBackToLogin **/
+    AS_Button_a5d5709a5a6d40b5b59566e65898b7b2: function AS_Button_a5d5709a5a6d40b5b59566e65898b7b2(eventobject) {
+        var self = this;
+        return self.goToLogin.call(this);
+    },
+    /** onTextChange defined for txtConfirmation **/
+    AS_TextField_f13b47716e9c4e57910f748fd332a1f8: function AS_TextField_f13b47716e9c4e57910f748fd332a1f8(eventobject, changedtext) {
+        var self = this;
+        return self.cleanConfPasswordError.call(this);
     },
     /** onOrientationChange defined for frmLogin **/
     AS_Form_bce8f418ade1455d8be2bdddf93637b1: function AS_Form_bce8f418ade1455d8be2bdddf93637b1(eventobject) {
         var self = this;
-    },
-    /** onTextChange defined for txtEmail **/
-    AS_TextField_i7e412883bbb4daf967005bf2fd4b72f: function AS_TextField_i7e412883bbb4daf967005bf2fd4b72f(eventobject, changedtext) {
-        var self = this;
-        return self.cleanEmailError.call(this);
-    },
-    /** onClick defined for btnBackToLogin **/
-    AS_Button_b3069b31a6844a21a116f80652eac004: function AS_Button_b3069b31a6844a21a116f80652eac004(eventobject) {
-        var self = this;
-        return self.goToLogin.call(this);
-    },
-    /** onTextChange defined for txtPassword **/
-    AS_TextField_d984052da1784ce5a9253faa6566876e: function AS_TextField_d984052da1784ce5a9253faa6566876e(eventobject, changedtext) {
-        var self = this;
-        return self.cleanPasswordError.call(this);
-    },
-    /** onTextChange defined for txtConfirmation **/
-    AS_TextField_e312c4ae7eca431599e8532894e76484: function AS_TextField_e312c4ae7eca431599e8532894e76484(eventobject, changedtext) {
-        var self = this;
-        return self.cleanConfPasswordErro.call(this);
     }
 });
